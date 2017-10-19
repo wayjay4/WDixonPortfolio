5e697f1e11a94d16429e322e5e360a041ed60570
wayjay4 <wayjay4@yahoo.com>
2017-10-19 06:17:55 +0000

    added mobirise assets and updated welcome landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,97 +8,93 @@
         <!-- Needed for ReactJS -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <!-- Site made with Mobirise Website Builder v3.12.1, https://mobirise.com -->
+        <meta name="generator" content="Mobirise v3.12.1, mobirise.com">
+        <meta name="description" content="">
+        <link href="vendor/mobirise/assets/images/logo.png" rel="shortcut icon" type="image/x-icon">
+
+        <title>Waylon Dixon Homepage</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
         <!-- Styles -->
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="vendor/mobirise/assets/bootstrap-material-design-font/css/material.css" rel="stylesheet" type="text/css">
+        <link href="vendor/mobirise/assets/tether/tether.min.css" rel="stylesheet" type="text/css">
+        <link href="vendor/mobirise/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="vendor/mobirise/assets/dropdown/css/style.css" rel="stylesheet" type="text/css">
+        <link href="vendor/mobirise/assets/animate.css/animate.min.css" rel="stylesheet" type="text/css">
+        <link href="vendor/mobirise/assets/theme/css/style.css" rel="stylesheet" type="text/css">
+        <link href="vendor/mobirise/assets/mobirise/css/mbr-additional.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+      <section id="menu-0">
+          <nav class="navbar navbar-dropdown bg-color transparent navbar-fixed-top">
+              <div class="container">
+                  <div class="mbr-table">
+                      <div class="mbr-table-cell">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                          <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                              <div class="hamburger-icon"></div>
+                          </button>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                          <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar">
+                            <li class="nav-item"><a class="nav-link link" href="/">HOME</a></li>
+                            <li class="nav-item"><a class="nav-link link" href="http://waylondixon.com/about_me.html">ABOUT ME</a></li>
+                            <li class="nav-item"><a class="nav-link link" href="http://waylondixon.com/skills_experience.html">EXPERIENCE AND SKILLS</a></li>
+                            <li class="nav-item"><a class="nav-link link" href="http://waylondixon.com/mywork.html">MY WORKS</a></li>
+                          </ul>
 
-                <div id="root"></div>
-            </div>
-        </div>
+                          <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                              <div class="close-icon"></div>
+                          </button>
+                      </div>
+                  </div>
+              </div>
+          </nav>
+      </section>
 
-        <script src="{{asset('js/app.js')}}" ></script>
+      <section class="mbr-section mbr-section-hero mbr-section-full mbr-parallax-background" id="header1-1" style="background-image: url(vendor/mobirise/assets/images/mbr-2000x662.jpg);">
+          <div class="mbr-table-cell">
+              <div class="container">
+                  <div class="row">
+                      <div class="mbr-section col-md-10 col-md-offset-1 text-xs-center">
+
+                          <h1 class="mbr-section-title display-1">Hi, <br>I'm Waylon, <br>web developer.</h1>
+
+                          <p class="mbr-section-lead lead">Front End Developer | Back End Developer</p>
+
+                          <div class="mbr-section-btn">
+                            <a class="btn btn-lg btn-primary" href="http://waylondixon.com/about_me.html">About Me</a>
+                            <a class="btn btn-lg btn-primary-outline btn-primary" href="http://waylondixon.com/skills_experience.html">Experience and Skills</a>
+                            <a class="btn btn-lg btn-primary-outline btn-primary" href="http://waylondixon.com/mywork.html">My Work</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+
+      <footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-2" style="background-color: rgb(50, 50, 50); padding-top: 1.75rem; padding-bottom: 1.75rem;">
+          <div class="container">
+              <p class="text-xs-center">Copyright (c) 2017 Waylon Dixon.</p>
+          </div>
+      </footer>
+
+      <script src="{{asset('js/app.js')}}" ></script>
+      <script src="vendor/mobirise/assets/web/assets/jquery/jquery.min.js" ></script>
+      <script src="vendor/mobirise/assets/tether/tether.min.js" ></script>
+      <script src="vendor/mobirise/assets/bootstrap/js/bootstrap.min.js" ></script>
+      <script src="vendor/mobirise/assets/smooth-scroll/smooth-scroll.js" ></script>
+      <script src="vendor/mobirise/assets/dropdown/js/script.min.js" ></script>
+      <script src="vendor/mobirise/assets/touch-swipe/jquery.touch-swipe.min.js" ></script>
+      <script src="vendor/mobirise/assets/viewport-checker/jquery.viewportchecker.js" ></script>
+      <script src="vendor/mobirise/assets/jarallax/jarallax.js" ></script>
+      <script src="vendor/mobirise/assets/theme/js/script.js" ></script>
+
+      <input name="animation" type="hidden">
     </body>
 </html>
